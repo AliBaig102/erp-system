79e4059dd7ce64c31bdbf3da7eba8346c981f529
NowPoshAliBaig <nowposh36@gmail.com>
2025-05-10 00:47:30 +0500

    feat: update middleware to handle access token validation and redirect logic for login and dashboard routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,16 +2,20 @@ import { NextRequest, NextResponse } from 'next/server'
 
 export function middleware(request: NextRequest) {
 	const path = request.nextUrl.pathname
-	// const accessToken = request.cookies.get('accessToken')?.value
-	// const publicApiPaths: string[] = ['/api/auth/login','/api/auth/unauthorized', '/api/language']
-	//
-	// if (!accessToken && !publicApiPaths.includes(path)) {
-	// 	return NextResponse.redirect(new URL('/api/auth/unauthorized', request.url))
-	// }
+	const accessToken = request.cookies.get('accessToken')?.value
+	const publicApiPaths: string[] = ['/login']
+
+	if (!accessToken && !publicApiPaths.includes(path)) {
+		return NextResponse.redirect(new URL('/login', request.url))
+	}
+
+	if (accessToken && publicApiPaths.includes(path)) {
+		return NextResponse.redirect(new URL('/dashboard', request.url))
+	}
 
 	return NextResponse.next()
 }
 
 export const config = {
-	matcher: ['/api/:path*'],
+	matcher: ['/login', '/', '/dashboard/:path*'],
 }
