e29b3a23f5820ff03269098b185cb3266232239b
NowPoshAliBaig <nowposh36@gmail.com>
2025-05-21 16:25:41 +0500

    fix: refine language retrieval logic in getLanguageClient function to ensure correct language detection from cookies

--- src/lib/frontend/clientCookie.ts
+++ src/lib/frontend/clientCookie.ts
@@ -1,7 +1,7 @@
 const getLanguageClient = (): 'ur' | 'en' => {
 	if (typeof document !== 'undefined') {
-		const lang = document.cookie.split('lang=')[1]
-		return lang ? 'ur' : 'en'
+		const lang = document.cookie.split('lang=')[1]?.split(";")[0]
+		return lang === 'ur' ? 'ur' : 'en'
 	}
 	return 'en'
 }
