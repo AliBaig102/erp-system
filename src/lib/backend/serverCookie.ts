28d7471cbe50ef12d9b273a2b81322d2ce92aa3b
NowPoshAliBaig <nowposh36@gmail.com>
2025-05-21 16:25:26 +0500

    fix: correct language retrieval logic in getLanguage function to ensure accurate language return

--- src/lib/backend/serverCookie.ts
+++ src/lib/backend/serverCookie.ts
@@ -3,7 +3,7 @@ import { cookies } from 'next/headers'
 export const getLanguage =async ():Promise<"en" | "ur"> => {
 	const cookiesStore = await cookies();
 	const lang = cookiesStore.get('lang')?.value;
-	return lang ? 'ur': 'en';
+	return lang === 'ur' ? 'ur' : 'en';
 }
 
 
